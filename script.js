698e6f079cd466a3acac54e68e959ca9940bde78
Stanislav <stif@tut.by>
2021-05-01 15:13:59 +0300

    refactor: clear code

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function removeTransition(e) {
-  console.log(e.propertyName);
   if (e.propertyName !== 'transform') return;
   e.target.classList.remove('playing');
 }
 
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
